ffbf9029c7d17b98b4a309cc1f933c3bf0973f6e
ahomyakov <ahomyakov@localhost6.localdomain6>
2011-11-15 13:05:42 +0300

    Инициализация слоёв случайными весами

--- neuro-commons/src/main/scala/ru/ahomyakov/neuro/perseptron/Layer.scala
+++ neuro-commons/src/main/scala/ru/ahomyakov/neuro/perseptron/Layer.scala
@@ -2,13 +2,29 @@ package ru.ahomyakov.neuro.perseptron
 
 import com.mongodb.DBObject
 import com.mongodb.casbah.commons.MongoDBObject
+import util.Random
 
 class Layer(weights: Array[Array[Double]],
             aFunc: Double => Double) {
+  /**
+   * Инициализация слоя из сохранённого в б/д json-объекта
+   */
   def this(o: DBObject, aFunc: Double => Double) =
     this (o.get("matrix").toString.split("::").
       map(col => col.split(":").
       map(element => java.lang.Double.parseDouble(element))), aFunc);
+
+  /**
+   * Инициализация слоя указанной размерности случайными весами
+   */
+  def this(inputs: Int,
+           outputs: Int,
+           aFunc: Double => Double) =
+    this (
+      (for (i <- 0 to outputs - 1) yield
+        (for (j <- 0 to inputs - 1) yield
+          scala.math.abs(Random.nextDouble()) % 1D).
+          toArray).toArray, aFunc);
 
   /**
    * Объект для сохранения в mongo
@@ -34,12 +50,12 @@ class Layer(weights: Array[Array[Double]],
   /**
    * Обратное распространение ошибки
    */
-//  protected def errorBackTrace(input: Array[Double],
-//                               requiredOutput: Array[Double],
-//                               realOutput: Array[Double],
-//                               teachingCoeff: Double): Layer =
-//  new Layer(weights.map(col => ),aFunc);
-//    for(i <- 0 to requiredOutput.length-1) yield
-//      (requiredOutput(i)-realOutput(i))*teachingCoeff
+  //  protected def errorBackTrace(input: Array[Double],
+  //                               requiredOutput: Array[Double],
+  //                               realOutput: Array[Double],
+  //                               teachingCoeff: Double): Layer =
+  //  new Layer(weights.map(col => ),aFunc);
+  //    for(i <- 0 to requiredOutput.length-1) yield
+  //      (requiredOutput(i)-realOutput(i))*teachingCoeff
 
 }
--- neuro-commons/src/main/scala/ru/ahomyakov/neuro/perseptron/Perseptron.scala
+++ neuro-commons/src/main/scala/ru/ahomyakov/neuro/perseptron/Perseptron.scala
@@ -10,6 +10,15 @@ class Perseptron(dao: MongoDao, layers: Array[Layer]) {
     this (dao, dao.findById(id).map(o => new Layer(o, (x => x): (Double => Double))).toArray);
 
   /**
+   * Инициализация персептрона слоями заданной размерности со случайными весами.
+   * Размерность задана как {{вх1, вых1}, {вх2, вых2},...}
+   * Контроля размерности нет
+   */
+  def this(dao: MongoDao, dimensions: Array[Array[Int]]) = this (dao,
+    dimensions.map(dim => new Layer(dim(0), dim(1),
+      (x => x): (Double => Double))));
+
+  /**
    * f2(f1(input*A_1)*A_2)....
    */
   def process(input: Array[Double]): Array[Double] =
@@ -24,16 +33,16 @@ class Perseptron(dao: MongoDao, layers: Array[Layer]) {
   /**
    * Обратное распространение ошибки
    */
-//  protected def errorBackTrace(requiredOutput: Array[Double],
-//                               realOutput: Array[Double],
-//                               teachingCoeff: Double): Perseptron =
-//    layers.foldRight(new Perseptron(dao, Array[Layer]))(
-//      (l: Layer, p: Perseptron) => (p.addLayerToBegin(l.errorBackTrace())));
+  //  protected def errorBackTrace(requiredOutput: Array[Double],
+  //                               realOutput: Array[Double],
+  //                               teachingCoeff: Double): Perseptron =
+  //    layers.foldRight(new Perseptron(dao, Array[Layer]))(
+  //      (l: Layer, p: Perseptron) => (p.addLayerToBegin(l.errorBackTrace())));
 
   /**
    * сделать из персептрона новый с добавлением слоя в начало.
    * используется при обратном распространении ошибки
    */
-  def addLayerToBegin(layer: Layer): Perseptron =
-    new Perseptron(dao, layer :: layers);
+//  def addLayerToBegin(layer: Layer): Perseptron =
+//    new Perseptron(dao, layer :: layers);
 }
