16053b93199c4f7110be8f6674e890efcf6c4a1f
and_hom <and_hom@localhost6.localdomain6>
2011-11-18 01:03:24 +0300

    Работа над обратным распространением

--- neuro-commons/src/main/scala/ru/ahomyakov/neuro/perseptron/Perseptron.scala
+++ neuro-commons/src/main/scala/ru/ahomyakov/neuro/perseptron/Perseptron.scala
@@ -1,6 +1,7 @@
 package ru.ahomyakov.neuro.perseptron
 
 import ru.ahomyakov.neuro.data.MongoDao
+import scala.Array
 
 class Perseptron(dao: MongoDao, layers: Array[Layer]) {
   /**
@@ -42,16 +43,34 @@ class Perseptron(dao: MongoDao, layers: Array[Layer]) {
     new Perseptron(dao, errorBackTrace(input, requiredOutput, layers.toList).toArray);
 
   /**
+   * Список выводов слоёв в обратном порядке. В конце вход сети.
+   */
+  protected def outputs(input: Array[Double]): List[Array[Double]] =
+    layers.foldLeft(List(input))(
+      (results: List[Array[Double]], layer: Layer) =>
+        (layer.apply(results.head) :: results));
+
+  /**
+   * Обратное распространение ошибки. В конце списка ошибка всей сети
+   */
+//  protected def errs(err: Array[Double],
+//                     teachingCoeff: Double,
+//                     outputs: List[Array[Double]]): List[Array[Double]] =
+//    layers.foldRight(List(err))((layer, errors) =>
+//      (layer.errorBackTrace(errors.head,) :: errors));
+
+
+  /**
    * Предварительно считаем все вектора ошибок
    */
-  protected def errs(err: Array[Double],
-                     teachingCoeff: Double,
-                     layersReversed: List[Layer]): List[Array[Double]] =
-    layersReversed match {
-      case Nil => Nil;
-      case head :: tail => head.errorBackTrace(err) ::
-        errs(head.errorBackTrace(err), teachingCoeff, tail);
-    };
+  //  protected def errs(err: Array[Double],
+  //                     teachingCoeff: Double,
+  //                     layersReversed: List[Layer]): List[Array[Double]] =
+  //    layersReversed match {
+  //      case Nil => Nil;
+  //      case head :: tail => head.errorBackTrace(err) ::
+  //        errs(head.errorBackTrace(err), teachingCoeff, tail);
+  //    };
 
   protected def err(requiredOutput: Array[Double],
                     realOutput: Array[Double],
