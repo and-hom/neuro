46a79df580626390a77ccfec58813a6f342ac7ec
ahomyakov <ahomyakov@localhost6.localdomain6>
2011-11-18 15:33:42 +0300

    fix

--- neuro-commons/src/main/scala/ru/ahomyakov/neuro/data/Dao.scala
+++ neuro-commons/src/main/scala/ru/ahomyakov/neuro/data/Dao.scala
@@ -1,10 +1,9 @@
 package ru.ahomyakov.neuro.data
 
-import collection.mutable.Buffer
 import com.mongodb.{WriteResult, DBObject}
 
 abstract class Dao {
-  def findById(id: String): Buffer[DBObject];
+  def findById(id: String): Iterable[DBObject];
 
   def store(obs: List[DBObject]): WriteResult;
 }
