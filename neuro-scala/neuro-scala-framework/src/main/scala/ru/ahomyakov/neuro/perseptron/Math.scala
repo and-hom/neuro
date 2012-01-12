118b1c72c14c384a770f4e131162e35f2e30ad65
ahomyakov <ahomyakov@static.localhost>
2012-01-12 17:27:08 +0400

    Исправления работы сети

--- neuro-scala/neuro-scala-framework/src/main/scala/ru/ahomyakov/neuro/perseptron/Math.scala
+++ neuro-scala/neuro-scala-framework/src/main/scala/ru/ahomyakov/neuro/perseptron/Math.scala
@@ -3,7 +3,7 @@ package ru.ahomyakov.neuro.perseptron
 
 object Math {
   def sumV(v1: Array[Double], v2: Array[Double]): Array[Double] =
-    (for (i <- 0 to v1.length - 1) yield v1(i) - v2(i)).toArray;
+    (for (i <- 0 to v1.length - 1) yield v1(i) + v2(i)).toArray;
 
   def substituteV(v1: Array[Double], v2: Array[Double]): Array[Double] =
     (for (i <- 0 to v1.length - 1) yield v1(i) - v2(i)).toArray;
