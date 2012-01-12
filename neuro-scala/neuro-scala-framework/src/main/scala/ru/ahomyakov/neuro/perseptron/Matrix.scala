f11b1ffe3dca0aa98a9c3dece10b1d79b4843264
ahomyakov <ahomyakov@static.localhost>
2012-01-12 19:58:44 +0400

    Матрица вместо массива

---
+++ neuro-scala/neuro-scala-framework/src/main/scala/ru/ahomyakov/neuro/perseptron/Matrix.scala
@@ -0,0 +1,32 @@
+package ru.ahomyakov.neuro.perseptron
+
+import util.Random
+
+
+class Matrix(rows: Array[Array[Double]],
+             cols: Array[Array[Double]]) {
+
+  def this(rows: Array[Array[Double]]) = this (rows,
+    (for (j <- 0 to rows(0).length - 1) yield
+      (for (i <- 0 to rows.length - 1) yield
+        rows(i)(j)).toArray).toArray);
+
+  def this(rows: Int, cols: Int) = this ((
+    for (i <- 0 to rows - 1) yield
+      (for (j <- 0 to cols - 1) yield
+        Random.nextDouble() % 1D).toArray).toArray);
+
+  def serialize(): String = rows.foldLeft("")((str1, row) =>
+    (str1 + (if (str1.isEmpty) "" else "::") + row.foldLeft("")
+      ((str2, cell) => (str2 + (if (str2.isEmpty) "" else ":") + cell))));
+
+  def map[T](f: (Double => T)): T = rows.map(f);
+
+  def mapT[T](f: (Double => T)): T = cols.map(f);
+
+  def get(i: Int, j: Int) = rows(i)(j);
+
+  def width(): Int = cols.length;
+
+  def height(): Int = cols.length;
+}
