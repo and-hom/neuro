ef8811826a931d7d651c562f203fe487ffc30368
ahomyakov <ahomyakov@static.localhost>
2012-01-13 13:54:18 +0400

    Dao теперь trait (не знаю зачем)

--- neuro-scala/neuro-scala-framework/src/main/scala/ru/ahomyakov/neuro/data/Dao.scala
+++ neuro-scala/neuro-scala-framework/src/main/scala/ru/ahomyakov/neuro/data/Dao.scala
@@ -2,7 +2,7 @@ package ru.ahomyakov.neuro.data
 
 import com.mongodb.{WriteResult, DBObject}
 
-abstract class Dao {
+trait  Dao {
   def findById(id: String): Iterable[DBObject];
 
   def store(obs: List[DBObject]): WriteResult;
